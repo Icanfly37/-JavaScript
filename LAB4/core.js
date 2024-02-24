71ece9990bcd023edf98e9622de2f52e446de9d5
Icanfly37 <101133532+Icanfly37@users.noreply.github.com>
2024-02-24 21:58:31 +0700

    pre-send

---
+++ LAB4/core.js
@@ -0,0 +1,23 @@
+let stringer = "Hello, world!";
+console.log(stringer.length);
+console.log(stringer.substr(7,5));
+
+let arr = [];
+arr.push(1);
+arr.push(2);
+console.log(arr);
+console.log(arr.length);
+
+let myDate = new Date()
+console.log(myDate.getDate());
+console.log("E");
+console.log(myDate.getMonth()); // ค่า 0 = มกราคม ค่า 1 = กุมภาพันธ์
+console.log("I");
+console.log(myDate.getFullYear());
+console.log("O");
+
+Math.abs(-11)
+Math.floor(30.33333)
+Math.ceil(30.33333)
+Math.round(30.5)
+Math.round(30.4)
