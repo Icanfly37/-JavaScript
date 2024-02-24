ab5c2cb7cee1875d38001dfa529d9f4ebcf9436f
Icanfly37 <101133532+Icanfly37@users.noreply.github.com>
2024-02-24 16:56:28 +0700

    pre-send
    ครั้งแรกในการเขียน js ร่วมกับ html

---
+++ LAB1/core.js
@@ -0,0 +1,19 @@
+// Example
+// ลอง copy paste code นี้ ใน  developer tools ดู
+
+var i = "MyMoney";
+for (var i=0; i<3; i++) {
+ console.log(i);
+}
+console.log("Old Value: " + i);
+
+let j = "MyMoney";
+for (let j=0; j<3; j++) {
+ console.log(j);
+}
+console.log("Old Value: " + j);
+
+const k = "MyMoney";
+for (const k=0; k<3; k++) {
+}
+console.log(k);
---
+++ Lab1.1/core.js
@@ -0,0 +1,3 @@
+var tester = "hello world";
+
+console.log(tester);
